0fc2b43f7c732be12899fb55b7307dd3169b327c
Edward G Prentice <egp@egp.net>
2026-04-15 15:58:26 -0400

    update sketch to use new interface

--- examples/BasicRead/BasicRead.ino
+++ examples/BasicRead/BasicRead.ino
@@ -1,8 +1,24 @@
-// examples/BasicRead/BasicRead.ino V2
+// examples/BasicRead/BasicRead.ino V3
 
+#include <TCP1819.h>
 #include <TCP0465.h>
 
+namespace {
+constexpr uint8_t kOxygenSdaPin = 10;
+constexpr uint8_t kOxygenSclPin = 11;
+constexpr uint32_t kOxygenI2cHz = 100000;
+}
+
+BBI2C oxygenBus;
 TCP0465 oxygen;
+
+void setupOxygenBus() {
+  memset(&oxygenBus, 0, sizeof(oxygenBus));
+  oxygenBus.bWire = 0;  // bit-banged I2C
+  oxygenBus.iSDA = kOxygenSdaPin;
+  oxygenBus.iSCL = kOxygenSclPin;
+  I2CInit(&oxygenBus, kOxygenI2cHz);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -10,12 +26,14 @@ void setup() {
     delay(10);
   }
 
+  setupOxygenBus();
+
   Serial.println();
   Serial.println("TCP0465 BasicRead");
   Serial.println("Note: first power-up needs >5 minutes warm-up.");
   Serial.println("Note: long-unused modules are recommended to preheat for >24 hours.");
 
-  if (!oxygen.begin()) {
+  if (!oxygen.begin(oxygenBus)) {
     Serial.print("begin() failed: ");
     Serial.println(oxygen.errorString());
     while (true) {
@@ -28,7 +46,6 @@ void setup() {
 
 void loop() {
   float percentVol = 0.0f;
-
   if (oxygen.readOxygenPercent(percentVol)) {
     Serial.print("O2 concentration: ");
     Serial.print(percentVol, 1);
@@ -37,8 +54,7 @@ void loop() {
     Serial.print("readOxygenPercent() failed: ");
     Serial.println(oxygen.errorString());
   }
-
   delay(1000);
 }
 
-// examples/BasicRead/BasicRead.ino V2
+// examples/BasicRead/BasicRead.ino V3
